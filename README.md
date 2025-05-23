a42c8fecf780f272d92ade03a188a90ffabfc80b
dashodanger <>
2025-05-23 10:32:39 -0600

    Update some deps and license; add more notes to README

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # EDGE-classic-webplayer
 
+## Prep
+
+- Build EDGE-Classic Emscripten build with freedoom2.wad and blasphem.wad in the /preload folder. Verify that it works with the test site in the EC repo.
+  - Use emsdk vesrion 3.1.51
+  - You will need to add the "-sUSE_ES6_IMPORT_META=0" flag to the Emscripten.cmake file in the EC repo
+    - This is not present by default because emsdk does not support it anymore
+- Place the generated WASM and JS files in the /src folder of this repository
+  - They will also need to be copied to the /build folder if doing npm run serve to test
+
 ## CLI Commands
 
 - `npm install`: Installs dependencies
--- src/components/licenses/index.tsx
+++ src/components/licenses/index.tsx
@@ -21,13 +21,16 @@ const LicenseModal: FunctionComponent<{ onClose: () => void }> = ({ onClose }) =
 );
 
 const license = `===========================================================================================
+Apache License 2.0
+===========================================================================================
+
+glad2 Loader Generator (generated output) - Copyright (c) 2013-2020 The Khronos Group Inc. 
+
+===========================================================================================
 BSD 3-Clause License
 ===========================================================================================
 
-"16-Bit Game Station" soundfont (select melodic instruments)
-  Copyright (c) 2009 Beatnik, Inc
-
-ANGLE (Almost Native Graphics Layer Engine) - Copyright (c) 2018 Google Inc. and ANGLE Project Authors
+"16-Bit Game Station" soundfont (select melodic instruments) - Copyright (c) 2009 Beatnik, Inc
 
 AlmostEquals Google Test Framework Component - Copyright (c) 2005 Google, Inc.
 
@@ -35,18 +38,15 @@ Blasphemer (various assets) - Copyright (c) 2021 Contributors to the Blasphemer 
 
 Freedoom (various assets) - Copyright (c) 2001-2019 Contributors to the Freedoom project
 
-"FDDMX" instrument bank - Copyright (c) 2023 Contributors to the Freedoom project
-  Copyright (c) ConSiGno ("DMXOPL" instrument bank which formed the basis for FDDMX)
-
-libRAD (patches.cpp/radmidi.cpp, adapted from YMFMIDI) - Copyright (c) 2021-2023 Devin Acker
-
-Mod4Play library - Copyright (c) 2024 dashodanger
-  Copyright (c) 2022 Olav Sørensen
-
 minivorbis library - Copyright (c) 2020 Eduardo Bart
   Copyright (c) 2002-2020 Xiph.org Foundation
 
-ZDoom FName implementation - Copyright (c) 2005-2007 Randy Heit
+Mod4Play library - 	Copyright (c) 2024-2025 dashodanger
+					Copyright (c) 2022 Olav Sørensen
+
+OpalMIDI library - Copyright (c) 2025, Dashodanger
+				   Copyright (c) 2021-2024, Devin Acker
+				   Opal Emulation released into Public Domain by Reality Productions
 
 ===========================================================================================
 CC 1.0 Universal License
@@ -78,16 +78,13 @@ CC-BY-SA-4.0 International License
 GPL3 License
 ===========================================================================================
 
-"Apogee" instrument bank - Melodic instruments Copyright (C) 1994-1995 Apogee Software, Ltd.
-	Percussion instruments Copyright (C) 1999, 2000, 2001 Simon Peter, et al.
-
 AJBSP - Copyright (c) 2000-2023 Andrew Apted, et al
-	Copyright (c) 1994-1998 Colin Reed
-	Copyright (c) 1997-1998 Lee Killough
+	 	Copyright (c) 1994-1998 Colin Reed
+	 	Copyright (c) 1997-1998 Lee Killough
 
 COAL - Copyright (c) 2021-2024 The EDGE Team
-	Copyright (c) 2009-2021 Andrew Apted, et al
-  Copyright (c) 1996-1997 Id Software, Inc.
+	   Copyright (c) 2009-2021 Andrew Apted, et al
+       Copyright (c) 1996-1997 Id Software, Inc.
 
 DDF library - Copyright (c) 1999-2024 The EDGE Team
 
@@ -101,12 +98,10 @@ Fluidlite - Copyright (c) 2016 Robin Lobel
 
 SLADE (IMF->WAV conversion and sky drawing routines) - Copyright (c) 2008 - 2023 Simon Judd
 
-SuperFastHash - Copyright (c) 2004-2010 Paul Hsieh
-
 XMIDI library (MIDI conversion) - Copyright (c) 2015-2022 Vitaly Novichkov
-	Copyright (c) 2015-2016 WildMIDI Developers
-	Copyright (c) 2014 Bret Curtis
-	Copyright (c) 2001 Ryan Nunn
+								  Copyright (c) 2015-2016 WildMIDI Developers
+								  Copyright (c) 2014 Bret Curtis
+								  Copyright (c) 2001 Ryan Nunn
 
 ===========================================================================================
 HacX License
@@ -127,43 +122,40 @@ base64.lua - Copyright (c) 2018 Ilya Kolbin
 BW_Midi_Sequencer library - Copyright (c) 2015-2022 Vitaly Novichkov
 
 "C++ implementation of a fast Gaussian blur algorithm by Ivan Kutskir - Integer Version"
-	Copyright (C) 2017 Basile Fraboni
-	Copyright (C) 2014 Ivan Kutskir
+	- Copyright (C) 2017 Basile Fraboni
+	- Copyright (C) 2014 Ivan Kutskir
+
+Dear ImGUI - Copyright (c) 2014-2023 Omar Cornut
 
 debugger.lua -Copyright (c) 2023 Scott Lembcke and Howling Moon Software
-
-gl4es library - Copyright (c) 2016-2018 Sebastien Chevalier
-	Copyright (c) 2013-2016 Ryan Hileman
-
-glad library - Copyright (c) 2013-2022 David Herberth
 
 json.lua - Copyright (c) 2020 rxi
 
 Lua - Copyright (C) 1994-2023 Lua.org, PUC-Rio.
 
 miniz library - Copyright (c) 2013-2014 RAD Game Tools and Valve Software
-	Copyright (c) 2010-2014 Rich Geldreich and Tenacious Software LLC
+				Copyright (c) 2010-2014 Rich Geldreich and Tenacious Software LLC
 
 PL_MPEG library - Copyright(c) 2019 Dominic Szablewski
 
 PNPOLY algorithm - Copyright (c) 1970-2003 Wm. Randolph Franklin
 
-utf8proc library - Copyright (c) 2014-2021 Steven G. Johnson, Jiahao Chen, Tony Kelman, 
-	Jonas Fonseca, et al
+StringHash class - Copyright (c) 2024-2024 the rbfx project
+			       Copyright (c) 2008-2022 the Urho3D project.
 
 ===========================================================================================
 Public Domain
 ===========================================================================================
 
-dr_wav, dr_mp3, and dr_flac libraries - David Reid
-
 Fraction.hpp - Bisqwit
 
-libRAD (opal.cpp/radplay.cpp) - Reality Productions
+Miniaudio library - David Reid
+
+prns.h - Marc B. Reynolds
 
 "sf_GMbank" soundfont (renamed to Default.sf2) - The Csound Developers
 
-stb_image, stb_image_write, stb_rect_pack and stb_truetype libraries - Sean Barrett
+stb_image, stb_rect_pack, stb_sprintf and stb_truetype libraries - Sean Barrett
 
 ===========================================================================================
 SIL Open Font License
@@ -172,10 +164,10 @@ SIL Open Font License
 Fira Code (renamed to ECFONT02.ttf) - Copyright 2014-2020 The Fira Code Project Authors
 
 Germania One (renamed to ECFONT03.ttf) - Copyright (c) 2011, John Vargas Beltrán,
-	with Reserved Font Name "Germania One"
+										 with Reserved Font Name "Germania One"
 
 Russo One (renamed to ECFONT01.ttf) - Copyright (c) 2011-2012, Jovanny Lemonad,
-	with Reserved Font Name "Russo"
+									  with Reserved Font Name "Russo"
 
 ===========================================================================================
 WidePix License
@@ -187,9 +179,9 @@ WidePix (various assets) - Copyright (c) 2020-2021 Nash Muhandes
 zlib License
 ===========================================================================================
 
-LOVE2D (Windows swapbuffers code) - Copyright (c) 2006-2024 LOVE Development Team
-
 SDL2 library - Copyright (c) 1997-2020 Sam Lantinga
+
+Sokol libraries - Copyright (c) 2018 Andre Weissflog
 `
 
 export default LicenseModal;
